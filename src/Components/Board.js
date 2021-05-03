3dd424516c426d72bb615ae8f72f2347371f5f8b
Thanos Katrakis <johndoe@example.com>
2021-05-03 22:23:52 +0300

    Basic functionality onClick event inside boxes

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -5,14 +5,13 @@ import Square from './Square'
 // We create a react class component
 export default class Board extends Component {
     renderSquare(i) {
-        return <Square value={this.props.Square[i]}
+        return <Square value={this.props.squares[i]}
             onClick={() => this.props.onClick(i)}
         />
     }
     render() {
         return (
             <div>
-                {/* The boxes that create the tic tac toe game UI */}
                 <div className="border-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -28,6 +27,7 @@ export default class Board extends Component {
                     {this.renderSquare(7)}
                     {this.renderSquare(8)}
                 </div>
+
             </div>
         )
     }
