5ffc24b7ab5d8e619c56fcd674c9c64e012ec2fc
Thanos Katrakis <johndoe@example.com>
2021-05-09 23:53:23 +0300

    Navbar implementation

--- src/Components/MainComponent.jsx
+++ src/Components/MainComponent.jsx
@@ -6,7 +6,7 @@ export default class MainComponent extends Component {
     render() {
         return(
             <div>
-                
+                <NavBar />
                 <Game />
             </div>
         );
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,1 +1,24 @@
 import React, { Component } from 'react'
+import {MenuItems} from "./MenuItems"
+
+class NavBar extends Component {
+    render() {
+        return (
+            <nav className="NavbarItems">
+                   <ul className="nav-menu">
+                        {MenuItems.map((item, index) => {
+                            return (
+                                <li key={index}>
+                                    <a className={item.cName} href={item.url}>
+                                    {item.title}
+                                    </a>
+                                </li>
+                            )
+                        })}
+                   </ul>
+            </nav>
+        )
+    }
+}
+
+export default NavBar
